d30a627d0ed9cf40277147cfa7b541a28e981bef
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-14 20:23:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(currentLine, person) {
-  function welcomeMessage.push("Welcome, " + person + " You are number " + currentLine + " in line.")
+  var welcomeMessage = ("Welcome, " + person + " You are number " + currentLine + " in line.")
   return welcomeMessage;
 }
